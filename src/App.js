8972e61c76d94d6afd7a8152d5ce4dfcd0855967
seunbayo <seunbayo83@gmail.com>
2022-05-06 14:05:49 +0100

    added connect button

--- src/App.js
+++ src/App.js
@@ -1,29 +1,88 @@
 import * as React from "react";
-import { ethers } from "ethers";
+import  { useEffect, useState } from "react";
+// import { ethers } from "ethers";
 import './App.css';
 
-export default function App() {
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
 
-  const wave = () => {
-    
-  }
-  
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      /*
+       * Check if we're authorized to access the user's wallet
+       */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /**
+   * Implement your connectWallet method here
+   */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
-        <div className="header">
-        👋 Howfar
-        </div>
-
+        <div className="header">👋 Hey there!</div>
         <div className="bio">
-          <h2>I am seunbayo and I work as a Developer Advocate at Spheron. Connect your Ethereum wallet and wave at me!</h2>
+          <h2>I am seunbayo and i am a developer Advocate</h2>
         </div>
-
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
+        <button className="wavebutton" onClick={null}>
+          Wave at me
         </button>
+        {/*
+         * If there is no currentAccount render this button
+         */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
 }
+
+
+export default App;
